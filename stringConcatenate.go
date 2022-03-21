b85ffb13754c0beb9ab36030eadf79173cec2d6f
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-21 11:39:11 +0800

    StringConcatenate No Test needed

---
+++ stringConcatenate.go
@@ -0,0 +1,13 @@
+package main
+
+func stringConcatenate() {
+	list1 := [4]string{"ans", "wer", "is", "of"}
+	list2 := [4]string{"-", "+", "*", "/"}
+	list3 := [4]string{"5", "10", "4", "9"}
+	//Enter your code here
+
+}
+
+func main() {
+	stringConcatenate()
+}
